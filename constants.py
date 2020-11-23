3548a2d7098a4a6ee341400bb3dd341b7d7ff449
Joaquin <jjpb2000@gmail.com>
2020-11-23 19:38:30 +0000

    Basic Pokemon Game Only 2 Pokemons 1 attack each No EVS, No IVS

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+#Pokemon stats
+
+HP = "HP"
+ATTACK = "Attack"
+DEFENSE = "Defense"
+SPATK = "Sp.Attack"
+SPDEF = "Sp.Defense"
+SPEED = "Speed"
+
+PHYSICAL = "physical"
+SPECIAL = "special"
+
+#Command
+DO_ATTACK = "attack"
+DO_ATTACK_SELECTION = "selected_attack"
---
+++ models.py
@@ -0,0 +1,79 @@
+from constants import DO_ATTACK
+
+
+class Battle:
+
+    def __init__(self, pokemon1, pokemon2):
+        self.pokemon1 = pokemon1
+        self.pokemon2 = pokemon2
+        self.actual_turn = 0
+
+    def is_finished(self):
+        finished = self.pokemon1.current_hp <= 0 or self.pokemon2.current_hp <= 0
+        if finished:
+            self.print_winner()
+        return finished
+
+    def execute_turn(self, turn):
+        command1 = turn.command1
+        command2 = turn.command2
+        attack1 = None
+        attack2 = None
+        if DO_ATTACK in command1.action.keys():
+            attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
+        if DO_ATTACK in command2.action.keys():
+            attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
+
+        #DAMAGE FORMULA
+        self.pokemon2.current_hp -= attack1.power
+        self.pokemon1.current_hp -= attack2.power
+
+        self.actual_turn += 1
+
+    def print_current_status(self):
+        print(self.pokemon1.name + " has " + str(self.pokemon1.current_hp) + " left ")
+        print(self.pokemon2.name + " has " + str(self.pokemon2.current_hp) + " left ")
+
+    def print_winner(self):
+        if(self.pokemon1.current_hp <= 0 < self.pokemon2.current_hp > 0 ):
+            print(self.pokemon2.name + " won in " + str(self.actual_turn) + " turns ! ")
+        elif(self.pokemon2.current_hp <= 0 < self.pokemon1.current_hp > 0 ):
+            print(self.pokemon1.name + " won in " + str(self.actual_turn) + " turns ! ")
+        else:
+            print("Double K.O")
+
+class Pokemon:
+
+    def __init__(self, name, level, type1, type2):
+        self.name = name
+        self.level = level
+        self.type1 = type1
+        self.type2 = type2
+        self.attacks = [] # Vector de ataques
+        self.stats = {}
+        self.current_status = 0
+        self.current_hp = 0
+
+class Attack:
+
+    def __init__(self, name, t, category, pp, power, accuracy):
+        self.name = name
+        self.t = t
+        self.category = category
+        self.pp = pp
+        self.power = power
+        self.accuracy = accuracy
+
+class Turn:
+
+    def __init__(self):
+        self.command1 = None
+        self.command2 = None
+
+    def can_start(self):
+        return self.command1 is not None and self.command2 is not None
+
+class Command:
+
+    def __init__(self,action):
+        self.action = action
